e21eea93ebfd2322eb90ad1e59e4dddc87f9c4c2
David Alberto Montaño Fetecua <davidmontano@seven4n.com>
2018-11-19 18:01:50 -0500

    New endpoint with hostname

--- simpleRest.go
+++ simpleRest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -22,6 +23,11 @@ func versionHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Version: %s", version)
 }
 
+func hostnameHandler(w http.ResponseWriter, r *http.Request) {
+	hostname, _ := os.Hostname()
+	fmt.Fprintf(w, "Hostname: %s", hostname)
+}
+
 func main() {
 
 	port := flag.String("port", "8080", "The port in which the microservice is running.")
@@ -29,6 +35,7 @@ func main() {
 
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/version", versionHandler)
+	http.HandleFunc("/hostname", hostnameHandler)
 	http.Handle("/metrics", prometheus.Handler())
 
 	log.Printf("Running on port %s\n", *port)
